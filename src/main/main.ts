3df237ee36efd8e4837caf9be02c9d9bb91386a3
Leonardo Victoriano <leon4rdo.dev@gmail.com>
2026-02-20 14:39:01 -0400

    fix: update production load path and refresh branding assets

--- src/main/main.ts
+++ src/main/main.ts
@@ -39,7 +39,7 @@ async function createWindow() {
         mainWindow.loadURL('http://localhost:5173');
         // mainWindow.webContents.openDevTools();
     } else {
-        mainWindow.loadFile(path.join(__dirname, '../../../dist/index.html'));
+        mainWindow.loadFile(path.join(__dirname, '../../dist/index.html'));
     }
 }
 
